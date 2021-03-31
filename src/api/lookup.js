78db9c76ca95d54b6d227a5fc551895deff1b2a1
dungpk14789 <62883445+dungpk14789@users.noreply.github.com>
2021-03-31 20:18:06 +0700

    add lookup

--- src/api/lookup.js
+++ src/api/lookup.js
@@ -1,5 +1,88 @@
 class LookUP {
+    constructor(form,url) {
+            this.form = form;
+            this.url = url;
+            this.searchDes = document.querySelector(".search-des");
+            this.spinnerSearch = document.querySelector(".spinner-search");
+            this.alertSearch = document.querySelector(".alert-search");
+            this.searchWord = document.querySelector(".search-word");
+            this.searchCategory = document.querySelector(".search-category");
+            this.vietnameseMeaning = document.querySelector(".vietnameseMeaning");
+            this.englishMeaning = document.querySelector(".englishMeaning");
+            this.example = document.querySelector(".example");
+            this.inputLookup = document.querySelector(".inputLookUp");
+    }
+    init() {
+       this.lookupSubmit();
+    }
+
+    lookupSubmit() {
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const inputSearch = this.inputLookup.value;
+            this.spinnerSearch.classList.remove("d-none");
+            fetch(this.url)
+            .then((d) => d.json())
+            .then((d) => {
+                this.resetUI(d[0].data, inputSearch);
+                this.form.reset();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        });
+    }
+
+    resetUI(dataArr, input) {
+        let temp = 0;
+        dataArr.forEach((data) => {
+            if (data.Terms === input) {
+                temp++;
+                this.searchDes.classList.remove("d-none");
+                this.spinnerSearch.classList.add("d-none");
+                this.alertSearch.classList.add("d-none");
+                this.searchWord.textContent = data.Terms;
+                this.searchCategory.textContent = data.Category;
+                this.vietnameseMeaning.textContent = data.VietnameseMeaning;
+                this.englishMeaning.textContent = data.EnglishMeaning;
+                this.example.textContent = data.Example;
+                return;
+            }
+        });
+
+        if(temp === 0) {
+            this.searchDes.classList.add("d-none");
+            this.spinnerSearch.classList.add("d-none");
+            this.alertSearch.classList.remove("d-none");
+
+            const alertWord = this.alertSearch.querySelector(".alert-word");
+            alertWord.textContent = input;
+
+            this.addWord(alertWord.textContent);
+        }
+    }
+
+    addWord(word) {
+        const btnSearchAdd = document.querySelector(".btn-search-add");
+            btnSearchAdd.addEventListener("click", (e) => {
+            const inputAdd = document.querySelector(".inputAdd");
+            inputAdd.value = word;
+
+            const spinSearchAdd = document.querySelector("#btnSpin");
+            const textSearchAdd = document.querySelector("#btnText");
+
+            textSearchAdd.textContent = "Adding...";
+            spinSearchAdd.classList.remove("d-none");
+
+            setTimeout(() => {
+                textSearchAdd.textContent = "Add";
+                spinSearchAdd.classList.add("d-none");
+              }, 2000);
+
+              console.log(inputAdd.value);
+        });
+    }
 
 }
 
-export { LookUP as default };
+export default LookUP
--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,12 @@
 import Tabs from "./ui/tabs";
 import Add from "../src/api/add";
+import LookUP from "../src/api/lookup";
 const tabs = new Tabs(document.querySelector(".function"));
 tabs.init();
 
 const url = "https://script.google.com/macros/s/AKfycbzT7M4Oy1mSuFnSVrp5142ZbWrz1UlSfU9LFEQL5NlPvc0ymUX-ITuLxNjugyXFePfZ/exec";
 const add = new Add(url, document.querySelector(".addForm"));
 add.afterSubmit();
+
+const lookup = new LookUP(document.querySelector(".form-lookup"),url);
+lookup.init();
