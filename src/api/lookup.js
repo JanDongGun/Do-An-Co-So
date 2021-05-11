649651d05c985a7a3b116bf1a9df3ab670174d3d
hoanganhquang <anhquang976@gmail.com>
2021-05-11 22:00:06 +0700

    updated lookup.js

--- src/api/lookup.js
+++ src/api/lookup.js
@@ -1,20 +1,20 @@
 class LookUP {
-    constructor(form,url) {
-            this.form = form;
-            this.url = url;
-            this.searchDes = document.querySelector(".search-des");
-            this.spinnerSearch = document.querySelector(".spinner-search");
-            this.alertSearch = document.querySelector(".alert-search");
-            this.searchWord = document.querySelector(".search-word");
-            this.searchCategory = document.querySelector(".search-category");
-            this.vietnameseMeaning = document.querySelector(".vietnameseMeaning");
-            this.englishMeaning = document.querySelector(".englishMeaning");
-            this.example = document.querySelector(".example");
-            this.inputLookup = document.querySelector(".inputLookUp");
+    constructor(form, url) {
+        this.form = form;
+        this.url = url;
+        this.searchDes = document.querySelector(".search-des");
+        this.spinnerSearch = document.querySelector(".spinner-search");
+        this.alertSearch = document.querySelector(".alert-search");
+        this.searchWord = document.querySelector(".search-word");
+        this.searchCategory = document.querySelector(".search-category");
+        this.vietnameseMeaning = document.querySelector(".vietnameseMeaning");
+        this.englishMeaning = document.querySelector(".englishMeaning");
+        this.example = document.querySelector(".example");
+        this.inputLookup = document.querySelector(".inputLookUp");
     }
     init() {
-       this.lookupSubmit();
-       this.searchUpdate();
+        this.lookupSubmit();
+        this.searchUpdate();
     }
 
     lookupSubmit() {
@@ -23,21 +23,21 @@ class LookUP {
             const inputSearch = this.inputLookup.value;
             this.spinnerSearch.classList.remove("d-none");
             fetch(this.url)
-            .then((d) => d.json())
-            .then((d) => {
-                this.resetUI(d[0].data, inputSearch);
-                this.form.reset();
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+                .then((d) => d.json())
+                .then((d) => {
+                    this.resetUI(d[0].data, inputSearch);
+                    this.form.reset();
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         });
     }
 
     resetUI(dataArr, input) {
         let temp = 0;
         dataArr.forEach((data) => {
-            if (data.Terms === input) {
+            if (data.Terms.toUpperCase() === input.toUpperCase()) {
                 temp++;
                 this.searchDes.classList.remove("d-none");
                 this.spinnerSearch.classList.add("d-none");
@@ -51,7 +51,7 @@ class LookUP {
             }
         });
 
-        if(temp === 0) {
+        if (temp === 0) {
             this.searchDes.classList.add("d-none");
             this.spinnerSearch.classList.add("d-none");
             this.alertSearch.classList.remove("d-none");
@@ -65,7 +65,7 @@ class LookUP {
 
     addWord(word) {
         const btnSearchAdd = document.querySelector(".btn-search-add");
-            btnSearchAdd.addEventListener("click", (e) => {
+        btnSearchAdd.addEventListener("click", (e) => {
             const inputAdd = document.querySelector(".inputAdd");
             inputAdd.value = word;
 
@@ -80,7 +80,7 @@ class LookUP {
                 textSearchAdd.textContent = "Add";
                 spinSearchAdd.classList.add("d-none");
                 addClick.click();
-              }, 2000);
+            }, 2000);
         });
     }
 
@@ -128,28 +128,27 @@ class LookUP {
         let valueInput = "";
         const inputUpdate = document.querySelector("#" + update + "Input");
 
-        if(update == "vnmMeaning"){
-            valueInput = this.searchWord.textContent + "|" + inputUpdate.value + "|" + this.englishMeaning.textContent + "|" + this.example.textContent; 
-        }else if (update == "engMeaning"){
+        if (update == "vnmMeaning") {
+            valueInput = this.searchWord.textContent + "|" + inputUpdate.value + "|" + this.englishMeaning.textContent + "|" + this.example.textContent;
+        } else if (update == "engMeaning") {
             valueInput = this.searchWord.textContent + "|" + this.vietnameseMeaning.textContent + "|" + inputUpdate.value + "|" + this.example.textContent;
-        }
-        else if (update == "exp") {
+        } else if (update == "exp") {
             valueInput = this.searchWord.textContent + "|" + this.vietnameseMeaning.textContent + "|" + this.englishMeaning.textContent + "|" + inputUpdate.value;
         }
-        
+
         btnUpdate.textContent = "Updating...";
 
         setTimeout(() => {
             fetch(this.url + `?func=edit&item=${valueInput}`, {
-                method: 'POST',
-                cache: 'no-cache',
-                redirect: 'follow',
-                body: JSON.stringify({})
-            })
-            .then(res => res.json())
-            .catch(err => {
-                console.log(err);
-            });
+                    method: 'POST',
+                    cache: 'no-cache',
+                    redirect: 'follow',
+                    body: JSON.stringify({})
+                })
+                .then(res => res.json())
+                .catch(err => {
+                    console.log(err);
+                });
 
 
             btnUpdate.textContent = "Update";
@@ -157,7 +156,7 @@ class LookUP {
             document.querySelector("#" + update + "Search").classList.remove("d-none");
 
             console.log(valueInput.split("|"));
-          }, 2000);
+        }, 2000);
     }
 }
 
