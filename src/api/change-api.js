053af616db0ca5b486612e79e1db8f5a3fb89a5d
hoanganhquang <anhquang976@gmail.com>
2021-06-05 20:55:56 +0700

    updated popup.html, style.css, change-api.js

--- src/api/change-api.js
+++ src/api/change-api.js
@@ -1,15 +1,36 @@
 class ChangeAPI {
     constructor() {
-        this.inputAPI = document.querySelector(".API")
-        this.btnAPI = document.querySelector(".btnAPI")
+        this.inputAPI = document.querySelector(".API");
+        this.btnAPI = document.querySelector(".btnAPI");
+        this.alertAddAPI = document.getElementById("alertAddAPI");
+        this.btnTextAPI = document.querySelector(".btnTextAPI");
+        this.btnSpinAPI = document.getElementById("btnSpinAPI");
+
     }
 
     setAPI() {
         this.inputAPI.value = JSON.parse(localStorage.getItem("apiKey"))
 
         this.btnAPI.addEventListener("click", () => {
+
+            this.btnTextAPI.innerHTML = "Adding...";
+            this.btnSpinAPI.classList.remove("d-none");
+            this.btnAPI.disabled = true;
+
             const valueAPI = this.inputAPI.value
             localStorage.setItem("apiKey", JSON.stringify(valueAPI))
+
+            setTimeout(()=>{
+                this.btnTextAPI.innerHTML = "Add API";
+                this.btnSpinAPI.classList.add("d-none");
+                this.btnAPI.disabled = false;
+                this.alertAddAPI.classList.remove("d-none");
+                setTimeout(()=>{
+                    this.alertAddAPI.classList.add("d-none");
+                }, 2000);
+            }, 2000);
+
+            
         })
     }
 }
